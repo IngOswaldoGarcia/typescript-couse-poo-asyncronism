01ca1ff61350758e28edb64c847dbfbdea9050db
Oswaldo Garcia <oswaldo.gs.developer@gmail.com>
2023-05-26 14:05:03 -0600

    Feat(3): Metodos

--- src/01-class.ts
+++ src/01-class.ts
@@ -12,7 +12,7 @@ console.log(date2);
 
 
 
-class MyDate {
+export class MyDate {
   year: number;
   month: number;
   day: number;
@@ -21,7 +21,6 @@ class MyDate {
     this.year = year;
     this.month = month;
     this.day = day;
-
   }
 }
 
