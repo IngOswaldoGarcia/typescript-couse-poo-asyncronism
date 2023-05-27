65dd555deee5f0169cc34a563cd6340aec6bd5e4
Oswaldo Garcia <oswaldo.gs.developer@gmail.com>
2023-05-26 19:05:52 -0600

    Feat(14): Singleton: Constructor privado

---
+++ src/13-singleton.ts
@@ -0,0 +1,29 @@
+export class MyService {
+
+  static instance: MyService | null = null;
+
+  private constructor(public name: string){}
+
+  getName() {
+    return this.name;
+  }
+
+  static create(name: string){
+    if(MyService.instance === null){
+      console.log('deberia entrar una vez');
+      MyService.instance = new MyService(name);
+    }
+    return MyService.instance;
+  }
+}
+
+const myService = MyService.create('service 1');
+console.log(myService.getName());
+
+const myService2 = MyService.create('service 2');
+console.log(myService2.getName());
+
+const myService3 = MyService.create('service 3');
+console.log(myService3.getName());
+
+console.log(myService === myService2);
