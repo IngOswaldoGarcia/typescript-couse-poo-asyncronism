3a0c13ea1ac8634d6a99c065ec21bd0ffb186074
Oswaldo Garcia <oswaldo.gs.developer@gmail.com>
2023-07-27 21:03:30 -0600

    Feat(18): Consumiendo ProductMemoryService

---
+++ src/app/02-main.ts
@@ -0,0 +1,22 @@
+import { ProductMemoryService } from './services/product-memory.service';
+
+const productService = new ProductMemoryService();
+
+productService.create({
+  title: 'Producto 1',
+  price: 100,
+  description: 'blah blah blah',
+  categoryId: 12,
+  images: []
+});
+
+const products = productService.getAll();
+const productId = products[0].id;
+
+productService.update(productId,{
+  title: 'nuevo nombre',
+});
+
+const rta = productService.findOne(productId)
+
+console.log(rta);
